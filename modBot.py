d7a05edd9113b50e51f6aa998b2809c9c7377b3b
lejutax <59413112+lejutax@users.noreply.github.com>
2020-06-08 19:47:29 +0200

    Update modBot.py

--- modBot.py
+++ modBot.py
@@ -122,6 +122,13 @@ async def evenement(ctx, a, b):
 async def evenement_error(ctx, error):
     if isinstance(error, commands.MissingAnyRole):
         await ctx.send('You need \"**`Dad`**\" or \"**`bot`**\" role.')
+        
+        
+        
+token = 't2NzE4MjczNDU4MTI2OTc5MDgy.Xt5xsw.B4M7uKFMu3j49p3ynqAFw08G3Rg'
+token_print = ''.join(token[2:])
+print (token_print)
 
-bot.run('NzE4MjczNDU4MTI2OTc5MDgy.Xt2VXg.jFj2F1s3Jzix14yx8seWvKLRLSw')
+
+bot.run(token_print)
 
